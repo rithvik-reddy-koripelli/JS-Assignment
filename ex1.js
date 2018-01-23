de58e345fd13e0fbe53d055166a898d8b6cb4212
rithvik-reddy-koripelli <rithvik.reddy@accoliteindia.com>
2018-01-23 17:19:20 +0530

    creating new repo for js assignment

---
+++ ex1.js
@@ -0,0 +1,81 @@
+(function() {
+	A();
+
+function C() {
+	console.log("OOPS!");
+}
+
+function E(f) {
+	console.log("E");
+	f();
+	var f = F;
+}
+
+function A() {
+	console.log("A");
+	B();
+};
+
+var C;
+
+function G() {
+	console.log("G");
+	H();
+
+	function H() {
+		console.log("H");
+		I();
+	};
+}
+
+var D = d;
+
+function d() {
+	console.log("D");
+	E(F);
+}
+
+function I() {
+	console.log("I");
+	J();
+	J();
+}
+
+function B() {
+	console.log("B");
+	C();
+};
+
+function F() {
+	console.log("F");
+	G();
+};
+
+function K(){
+var rest = "KLMNOPQRSTUVWXYZ".split("");
+for (var i=0; i<rest.length; i++) {
+	(function(i){
+		
+		window[rest[i]] = function() {
+			console.log(rest[i]);
+			if (i < (rest.length-1)) {
+				window[rest[i+1]]();
+			}
+		};
+	})(i);
+}
+};
+function J() {
+	J = function() {
+		console.log("J");
+		K();
+		window['K']();
+	};
+};
+
+function C() {
+	console.log("C");
+	d();
+};
+
+})();
